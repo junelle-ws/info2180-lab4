6692c858f6f324cab6aa1ef6542faf828d6edeb2
junelle-ws <Junellews@gmail.com>
2018-10-22 20:24:24 -0700

    first question

--- maze.js
+++ maze.js
@@ -1,13 +1,9 @@
 window.onload = function () {
-    var boundary1 = document.getElementById("boundary1");
-    boundary1.onmouseover = function () {
-        if (!hasClass(boundary1, "youlose")) {
+var x = document.getElementById("boundary1");
+   boundary1.onmouseover = function () {
             boundary1.className += " youlose";
         }
     }
-}
-function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-} 
+
 
 
