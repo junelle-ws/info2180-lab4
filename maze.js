41eaed9ec406247c6eb5c4fdcf0c03139e213552
junelle-ws <Junellews@gmail.com>
2018-10-23 11:19:22 -0700

    final question

--- maze.js
+++ maze.js
@@ -2,7 +2,8 @@ window.onload = function () {
     var boundary1 = document.getElementById("boundary1");
     var end = document.getElementById("end");
     var start = document.getElementById("start");
-    var status = document.getElementById("status")
+    var status = document.getElementById("status");
+    var maze = document.getElementById("maze")
     var boundary = document.getElementsByClassName("boundary");
     var x = document.querySelectorAll(".boundary");
     var i;
@@ -17,26 +18,32 @@ window.onload = function () {
     }
 
 
-  
+
     for (i = 0; i < boundary.length; i++) {
-        
+
         x[i].onmouseover = function () {
             //alert("you lose");
             status.innerHTML = "you lose";
             for (var e = 0; e < boundary.length; e++) {
                 x[e].className += " youlose";
-                
+
 
             }
         }
+
+    }
+    boundary1.onmouseover = function () {
+        boundary1.className += " youlose";
+
+    }
+    maze.onmouseleave = function () {
+        // boundary1.className += " youlose";
+
         
+        for (let j = 0; j < boundary.length; j++)
+            boundary[j].classList.add("youlose");
+        document.getElementById("status").innerHTML = "You Lose!"; // Ex 5
     }
-  boundary1.onmouseover = function () {
-      boundary1.className += " youlose";
-      alert("you lose");
-    }
-   
-   
+
 }
-
-
+    
