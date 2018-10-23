1b5fecd2aed6a98875444f81c231bd0c7fff2f74
junelle-ws <Junellews@gmail.com>
2018-10-23 08:48:55 -0700

    second transcript

--- maze.js
+++ maze.js
@@ -1,9 +1,20 @@
 window.onload = function () {
-var x = document.getElementById("boundary1");
-   boundary1.onmouseover = function () {
-            boundary1.className += " youlose";
+    var boundary1 = document.getElementById("boundary1");
+    var boundary = document.getElementsByClassName("boundary");
+    var x = document.querySelectorAll(".boundary");
+    var i;
+    for (i = 0; i < boundary.length; i++) {
+        x[i].onmouseover = function () {
+            for (var e = 0; e < boundary.length; e++) {
+                x[e].className += " youlose";
+            }
         }
     }
 
+  boundary1.onmouseover = function () {
+            boundary1.className += " youlose";
+    }
+   
+}
 
 
