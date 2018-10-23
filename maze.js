571d51f993ac6e311579d18734010c13f68d10c2
junelle-ws <Junellews@gmail.com>
2018-10-23 10:16:39 -0700

    question 5

--- maze.js
+++ maze.js
@@ -2,27 +2,40 @@ window.onload = function () {
     var boundary1 = document.getElementById("boundary1");
     var end = document.getElementById("end");
     var start = document.getElementById("start");
+    var status = document.getElementById("status")
     var boundary = document.getElementsByClassName("boundary");
     var x = document.querySelectorAll(".boundary");
     var i;
 
     end.onmouseover = function () {
-        alert("You Win!!!!");
+        //alert("You Win!!!!");
+        status.innerHTML = "you win";
     }
     start.onclick = function () {
         location.reload();
 
     }
+
+
+  
     for (i = 0; i < boundary.length; i++) {
+        
         x[i].onmouseover = function () {
+            //alert("you lose");
+            status.innerHTML = "you lose";
             for (var e = 0; e < boundary.length; e++) {
                 x[e].className += " youlose";
+                
+
             }
         }
+        
     }
   boundary1.onmouseover = function () {
-            boundary1.className += " youlose";
+      boundary1.className += " youlose";
+      alert("you lose");
     }
+   
    
 }
 
