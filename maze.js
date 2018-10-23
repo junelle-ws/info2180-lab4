2952932226c164b8b87a851f2b26ed0d40796200
junelle-ws <Junellews@gmail.com>
2018-10-23 09:44:15 -0700

    fourth question

--- maze.js
+++ maze.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     var boundary1 = document.getElementById("boundary1");
     var end = document.getElementById("end");
+    var start = document.getElementById("start");
     var boundary = document.getElementsByClassName("boundary");
     var x = document.querySelectorAll(".boundary");
     var i;
@@ -8,7 +9,10 @@ window.onload = function () {
     end.onmouseover = function () {
         alert("You Win!!!!");
     }
-    
+    start.onclick = function () {
+        location.reload();
+
+    }
     for (i = 0; i < boundary.length; i++) {
         x[i].onmouseover = function () {
             for (var e = 0; e < boundary.length; e++) {
